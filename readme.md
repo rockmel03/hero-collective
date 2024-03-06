18e17c9c473e9e2bf85ad7ba9a84cc9226d88266
rockmel03 <kamalswag2@gmail.com>
2024-03-06 15:57:30 +0530

    added few more animations

--- readme.md
+++ readme.md
@@ -1,7 +1,6 @@
 ## Disclaimer
 
-This is clone of Hero collective all the Images, videos and assets are used only for development purposes.
-
+This is clone of Hero collective all the Images, videos, text content and assets are used only for development purposes.
 
 
 #  [Hero Collective](https://www.herocollective.co/)
@@ -10,6 +9,6 @@ This site is clone of Hero collective it is an awwards winning site at [awwards.
 
 I have created some of the major animations and UI of the wesite using:-
 
-* Tailwind Css - A utility classes framework 
+* Tailwind Css - A utility-first CSS framework packed with classes.
 * GSAP - A Javascripts animation liabrary used to make smooth animations.
 * GSAP-scrollTrigger - GSAP addon liabrary used to run animation on scrolling.
--- src/script.js
+++ src/script.js
@@ -1,86 +1,91 @@
 
-// import data from './data'
-
 const data = [
     {
         id: 1,
         title: "TIAA",
         description: "To move the needle on retirement preparedness in the black community, we had to move the needle on what a campaign is. We tapped a Hip Hop icon to put our message to music and kickstart a movement.",
-        imgURL: "https://images.prismic.io/hero-collective/65bc0cfe615e73009ec43657_header_bg_d.jpg?auto=format,compress&w=1200&fm=jpeg&q=80",
-        vdoURL: "https://hero-collective.cdn.prismic.io/hero-collective/65bc0ecf615e73009ec43661_Wyclef_Teaser.mp4"
+        imgURL: "header_bg_d.jpg",
+        vdoURL: "Wyclef_Teaser.mp4"
     },
     {
         id: 2,
         title: "Lenovo Yoga ",
         description: "We had to reposition the Lenovo Yoga series as an essential tool for Elite Gen Z consumers, so we created a campaign around the multi-talented T-Pain putting his Lenovo laptops to the test.",
-        imgURL: "https://images.prismic.io/hero-collective/65b16b36615e73009ec3e675_Lenovo-Yoga9i2in1-Phone-16x9-Generic_HeroTile_V2.jpg?auto=format,compress&w=1920&fm=webp&q=80",
-        vdoURL: "https://hero-collective.cdn.prismic.io/hero-collective/65b042d8615e73009ec3dd92_Lenovo-TPain-Homepage_Thumbnail.mp4"
+        imgURL: "Lenovo-Yoga9i2in1-Phone-16x9-Generic_HeroTile_V2.jpg",
+        vdoURL: "Lenovo-TPain-Homepage_Thumbnail.mp4"
     },
     {
         id: 3,
         title: "Lenovo",
         description: "Gen Z needed to see Lenovo as the culture-forward, premium brand they are. We told stories of diverse change-makers using Yoga laptops to create something greater than themselves and make lasting impacts in their communities.",
-        imgURL: "https://images.prismic.io/hero-collective/70313c08-2ee8-41e5-9ed2-eee9097cabc1_Screen+Shot+2023-01-24+at+11.10.12+AM+%281%29.jpg?auto=compress,format&w=1920&fm=webp&q=80",
-        vdoURL: "https://hero-collective.cdn.prismic.io/hero-collective/b8670d79-8d71-43c4-8c19-9e83a8348ed9_Lenovo+30+Youtube+16X9.mp4"
+        imgURL: "Lenovo30+Youtube+16X9.jpg",
+        vdoURL: "Lenovo30+Youtube+16X9.mp4"
     },
     {
         id: 4,
         title: "Google",
         description: "Google needed to become a brand that amplified Black+ voices. We built a social-first campaign celebrating conscious, community-minded creators who used Pixel to elevate their content and their culture.",
-        imgURL: "https://images.prismic.io/hero-collective/6576e7fa531ac2845a25e742_herogoogle.jpg?auto=format,compress&w=1920&fm=webp&q=80",
-        vdoURL: "https://hero-collective.cdn.prismic.io/hero-collective/6b3df641-93d2-4a4e-82be-0ea238f55a60_google_preview.mp4"
+        imgURL: "google_preview.jpg",
+        vdoURL: "google_preview.mp4"
     },
     {
         id: 5,
         title: "Band-aid",
         description: "Band-Aid‘s new multi-tone brand saw significant backlash during BLM. Our story-driven campaign creatively aligned OURTONE with the resiliency that defines Black people’s pursuit of progress, equity, and equality.",
-        imgURL: "https://images.prismic.io/hero-collective/6576e21f531ac2845a25e714_hero_band-aid.jpg?auto=format,compress&w=1920&fm=webp&q=80",
-        vdoURL: "https://hero-collective.cdn.prismic.io/hero-collective/1dfa44a3-f018-4761-aa87-9d22e9cbfb7f_01_Hero+Band-Aid+Preview+Candace.mp4"
+        imgURL: "01_Hero+Band-Aid+Preview+Candace.jpg",
+        vdoURL: "01_Hero+Band-Aid+Preview+Candace.mp4"
     },
     {
         id: 6,
         title: "Shell",
         description: "To stand out, Shell needed the idea of “performance” to resonate beyond the promise oftheir products. Our loyalty and gaming social programs, and our influencer series highlighted the lives of extreme achievers.",
-        imgURL: "https://images.prismic.io/hero-collective/6576e857531ac2845a25e744_HeroShell.jpg?auto=format,compress&w=1920&fm=webp&q=80",
-        vdoURL: "https://hero-collective.cdn.prismic.io/hero-collective/f8724f2a-5a9a-4916-bd27-1c85771bfd39_12_Shell-Vpower-Performanceunbound.mp4"
+        imgURL: "12_Shell-Vpower-Performanceunbound.avif",
+        vdoURL: "12_Shell-Vpower-Performanceunbound.mp4"
     },
     {
         id: 7,
         title: "NIKE",
         description: "Hero was tasked with communicating a brand identity for Nike’s Inclusive Communities Portfolio that imbued its mission with the bold, unapologetic tone of Nike, including a video for their Black Community Commitment program.",
-        imgURL: "https://images.prismic.io/hero-collective/07cff86a-b372-44f5-9ea0-b674980f1cff_nike+hero+3840x3622.jpg?auto=compress,format&w=1920&fm=webp&q=80",
-        vdoURL: "https://hero-collective.cdn.prismic.io/hero-collective/2f620d9e-2214-430f-8969-25739673c61d_Dear+Nike+v10.mp4"
+        imgURL: "Dear+Nike+v10.avif",
+        vdoURL: "Dear+Nike+v10.mp4"
     },
 ]
+
 const footerData = [
     {
         title: "about",
-        imgURL: "https://images.prismic.io/hero-collective/ba474a39-2476-4028-b5a3-906433f0613a_footer_about.jpg?auto=compress,format&w=375&fm=webp&q=80",
+        imgURL: "footer_about.jpg",
     },
     {
         title: "work",
-        imgURL: "https://images.prismic.io/hero-collective/b343fa07-97a4-433e-a030-ffa9d43fe633_footer_work.jpeg?auto=compress,format&w=375&fm=webp&q=80",
+        imgURL: "culture_hack_image+resized.avif",
     },
     {
         title: "healthcare",
-        imgURL: "https://images.prismic.io/hero-collective/b7ec086b-30eb-49e5-b492-72b74efbde69_footer_healthcare.jpg?auto=compress,format&w=375&fm=webp&q=80",
+        imgURL: "footer_healthcare.jpg",
     },
     {
         title: "latest",
-        imgURL: "https://images.prismic.io/hero-collective/6576e069531ac2845a25e6f0_footer_latest.jpg?auto=format,compress&w=375&fm=webp&q=80",
+        imgURL: "footer_latest.jpg",
     },
     {
         title: "contact",
-        imgURL: "https://images.prismic.io/hero-collective/350b91c6-e90b-4504-83f9-cd37e7556bc5_footer_contact.jpg?auto=compress,format&w=375&fm=webp&q=80",
+        imgURL: "footer_contact.avif",
     },
 ]
 
 locoScroll()
 heroAnimation()
-fetchData()
+page1Animation()
+playSecAnime()
+showData()
 // newsAnimation()
+latestSecAnime()
 cultureAnimation()
 footerAnimation()
+
+
+
 
 
 function locoScroll() {
@@ -127,10 +132,21 @@ function locoScroll() {
     ScrollTrigger.refresh();
 
 }
+
 function heroAnimation() {
     const tl = gsap.timeline();
     const videosEls = document.querySelectorAll('.hero__heading--top h1 video')
-
+    tl.from('nav h2 span ', {
+        opacity: 0,
+        x: '-100%',
+        duration: .5,
+        delay: 1,
+        stagger: .2,
+    })
+    tl.from('nav div ', {
+        opacity: 0,
+        duration: .5,
+    })
     tl.from(".hero__heading--top h1 div", {
         opacity: 0,
         duration: .5,
@@ -144,18 +160,78 @@ function heroAnimation() {
     tl.from(".hero__heading--top h1 video", {
         opacity: 0,
         duration: .5,
-        function: () => {
+        onstart: () => {
             videosEls.forEach((item) => item.play())
         }
     })
 }
-function fetchData() {
+
+function page1Animation() {
+
+    gsap.from(".page1__headings h1", {
+        opacity: .5,
+        y: "100%",
+        stagger: .2,
+        scrollTrigger: {
+            scroller: "main",
+            trigger: ".page1__headings h1",
+            start: "top 90%",
+            end: "top 20%",
+            scrub: 3,
+            // markers: true,
+        }
+    });
+}
+
+function playSecAnime() {
+    gsap.from(".heading--fade-left , .heading-sm--fade-left", {
+        x: "-110%",
+        duration: .5,
+        scrollTrigger: {
+            scroller: "main",
+            trigger: ".heading--fade-left , .heading-sm--fade-left ",
+            start: "top 70%",
+            end: "top 40%",
+            scrub: 2,
+            // markers: true,
+        }
+    });
+    gsap.from(".heading--fade-right , .heading-sm--fade-right", {
+
+        x: "110%",
+        duration: .5,
+        scrollTrigger: {
+            scroller: "main",
+            trigger: ".heading--fade-right , .heading-sm--fade-right ",
+            start: "top 70%",
+            end: "top 40%",
+            scrub: 2,
+            // markers: true,
+        }
+    });
+}
+
+function latestSecAnime() {
+    gsap.from('.bottom--in', {
+        y: '100%',
+        scrollTrigger: {
+            scroller: "main",
+            trigger: ".bottom--in",
+            start: "top 100%",
+            end: "top 70%",
+            scrub: 2,
+            // markers: true,
+        }
+    })
+}
+
+function showData() {
     data.forEach((item, index) => {
         document.querySelector(".data").innerHTML += `<section data-scroll data-scroll-speed="-1" data-id="${item.id}" class="latest rounded-[50px_50px_0px_0px] relative">
     <img
       class="w-full h-full object-cover absolute top-0 z-[2] rounded-[50px_50px_0px_0px]"
       loading="lazy"
-      src="${item.imgURL}"
+      src="./assets/Images/${item.imgURL}"
       alt=""
     />
     <div
@@ -165,11 +241,11 @@ function fetchData() {
       <p data-scroll data-scroll-speed="1"  class="latest__sub-heading text-[2em] uppercase">
       ${item.description}
       </p>
-      <p class="py-[5vh] w-full"><span>${index}/${data.length}</span></p>
+      <p class="py-[5vh] w-full"><span>${index + 1}/${data.length}</span></p>
       <div data-scroll data-scroll-speed="3" class="w-1/2 aspect-video p-2 bg-black rounded">
         <video
         loading="lazy"
-          src="${item.vdoURL}"
+          src="./assets/videos/${item.vdoURL}"
           class="w-full h-full object-cover"
           autoplay
           muted
@@ -205,9 +281,10 @@ function cultureAnimation() {
         scrollTrigger: {
             scroller: "main",
             trigger: ".culture",
-            start: '50% 120%',
-            end: "50% 90%",
+            start: 'top 70%',
+            end: "top 40%",
             scrub: 2,
+            // markers: true,
         }
     })
     gsap.from('.culture_heading--one img:nth-child(1)', {
@@ -248,15 +325,28 @@ function footerAnimation() {
     gsap.from('footer h1 div', {
         // opacity: 0,
         y: '50%',
-        stagger: .2,
+        stagger: .3,
         scrollTrigger: {
             scroller: "main",
             trigger: "footer",
-            start: '50% 110%',
-            end: "50% 80%",
+            start: 'top 80%',
+            end: "top 30%",
             scrub: 2,
         }
     })
+    gsap.from('footer .footer__links', {
+        opacity: 0,
+        scrollTrigger: {
+            scroller: "main ",
+            trigger: "footer",
+            start: '50% 80%',
+            end: "50% 30%",
+            scrub: 2,
+            // markers: true
+        }
+    })
+
+
 }
 
 
@@ -267,7 +357,7 @@ footerData.forEach((item, index) => {
     document.querySelector('.footer__links').innerHTML += `<div
     class="footer__img min-h-[10vh] relative flex-1 flex items-center justify-center"
   >
-    <img src="${item.imgURL}" alt="" class="w-full h-full object-cover" />
+    <img src="./assets/Images/${item.imgURL}" alt="" class="w-full h-full object-cover" />
     <h4 class="uppercase font-anton text-[3em] leading-none absolute cursor-pointer after:w-0 after:h-[2px] after:absolute after:top-full after:left-0 after:bg-white after:rounded after:duration-200 hover:after:w-full ">${item.title}</h4>
   </div>`
 })
